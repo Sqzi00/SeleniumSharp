e2957b8503779e4e997ec62cf9e7784660b50ec6
Sqzi00 <61646294+Sqzi00@users.noreply.github.com>
2022-02-27 18:10:42 +0100

    Add project files.

---
+++ SeleniumSharp/BaseClass/BaseTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumSharp.BaseClass
+{
+    public class BaseTest
+    {
+
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Open()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.facebook.com/";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElement(By.XPath("//button[@data-cookiebanner='accept_button']")).Click();
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+
+
+
+    }
+}
---
+++ SeleniumSharp/TestClass.cs
@@ -0,0 +1,58 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumSharp.BaseClass;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+namespace SeleniumSharp
+{
+    [TestFixture]
+    public class TestClass : BaseTest
+    {
+        [Test, Category("Smoke Testing")]
+        public void TestMethod1()
+        {
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            IWebElement registerButton = driver.FindElement(By.XPath(".//*[@data-testid='open-registration-form-button']"));
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(registerButton).Click().Perform();
+            Thread.Sleep(3000);
+            IWebElement monthDropDownList = driver.FindElement(By.XPath(".//*[@id='month']"));
+            SelectElement element = new SelectElement(monthDropDownList);
+            element.SelectByIndex(1); // Select by index
+            element.SelectByText("mar"); // Select by text
+            element.SelectByValue("6"); // Select by value
+
+
+        }
+
+        [Test, Category("Regression Testing")]
+        public void TestMethod2()
+        {
+
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+
+        }
+
+
+        [Test, Category("Smoke Testing")]
+        public void TestMethod3()
+        {
+
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Thread.Sleep(5000);
+        }
+
+
+
+    }
+}
